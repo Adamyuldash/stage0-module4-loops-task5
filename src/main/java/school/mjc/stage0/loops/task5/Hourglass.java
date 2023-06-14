14e83eb6bdd0c6a618a76e265267bbaf2557c83d
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-06-14 16:21:25 -0700

    123

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
 
     }
 }
